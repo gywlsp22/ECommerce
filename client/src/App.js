eb0912ba0f7c58f2c4a9ca7a0398b83c9edcc326
dlsvl <dlsvl98@daum.net>
2022-07-31 16:04:19 +0900

    Add bootstrap-UI-Framework

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import {BrowserRouter, Route, Routes,Link} from 'react-router-dom'
 import HomeScreen from './screeen/HomeScreen';
 import ProductScreen from './screeen/ProdctScreen';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
+
 
 
 
@@ -9,16 +13,29 @@ import ProductScreen from './screeen/ProdctScreen';
 function App() {
   return (
     <BrowserRouter>
-     <div>
+     <div className="d-flex flex-column site-container">
       <header>
-       <Link to="/">amazon</Link>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+            <Navbar.Brand>amazon</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
+       
       </header>
       <main>
+        <Container>
         <Routes>
           <Route path="/product/:slug" element={<ProductScreen />}/>
           <Route path="/" element={<HomeScreen />} />
         </Routes>
+        </Container>
       </main>
+
+      <footer>
+        <div className='text-center'>All rights reserved</div>
+      </footer>
     </div>
     </BrowserRouter>
    
